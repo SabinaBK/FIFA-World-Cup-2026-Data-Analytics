bb6d5534eb94833f5e21cf407847ed426f77cf52
Prasiddha Bhantana <ahddisarp.sh@gmail.com>
2026-09-03 23:39:27 +0930

    Remove unused sampling script

--- prasiddha/script/sampling.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("fifa_passing_cleaned.csv")
-
-high_accuracy = df[df["Passing Accuracy (%)"] >= 90]
-low_accuracy = df[df["Passing Accuracy (%)"] < 90]
-
-print("90% or greater:", len(high_accuracy))
-print("Less than 90%:", len(low_accuracy))
